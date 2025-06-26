bfedc676ab68371cfabcc464688661712b83cee7
cls <tanishqrimjha31.com>
2025-06-26 15:23:31 +0530

    add

--- Frontend/REACT/class9c2/src/utils/Data.jsx
+++ Frontend/REACT/class9c2/src/utils/Data.jsx
@@ -1,2 +1,2 @@
-const Data = ["Anshul Gangrade", "prachi", "Shubham", "Adarsh", "Anshul Patel", "Prachi Jajme", "Khusi Patel", "Sourabh", "Mohit", "Aviraj", "Harsh", "Jagmohan"];
+const Data = ["Anshul Gangrade","jaggu", "prachi", "Shubham", "Adarsh", "Anshul Patel", "Prachi Jajme", "Khusi Patel", "Sourabh", "Mohit", "Aviraj", "Harsh", "Jagmohan"];
 export default Data;
